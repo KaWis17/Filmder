5e75e17e68bc81e3c851034b77ba1b6505c4a482
kawis <krzymado@gmail.com>
2023-11-11 18:02:38 +0100

    I give up v2

--- FilmderAPP/App.js
+++ FilmderAPP/App.js
@@ -1,10 +1,51 @@
-import Navigation from './BottomTabNavigator';
-import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from "react/cjs/react.development"
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from './FirebaseConfig';
+
 
 export default function App() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function signIn(){
+    alert('SignIn message')
+  }
+
+  function signUp(){
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        alert('Success')
+
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+    });
+  }
+
   return (
-    <NavigationContainer>
-        <Navigation />
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Firebase auth!</Text>
+      <StatusBar style="auto" />
+      <TextInput placeholder='Email' value={email} onChangeText={(Text) => {setEmail(Text)}}></TextInput>
+      <TextInput placeholder='Password' value={password} onChangeText={(Text) => {setPassword(Text)}}></TextInput>
+      <Button title="submit" onPress={signUp}/>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- FilmderAPP/FirebaseConfig.js
+++ FilmderAPP/FirebaseConfig.js
@@ -1,19 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth'
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCVJISFJASvRrGNyuNGOcW6jF5QhgZwsOM",
-    authDomain: "filmderapp-621aa.firebaseapp.com",
-    projectId: "filmderapp-621aa",
-    storageBucket: "filmderapp-621aa.appspot.com",
-    messagingSenderId: "141226414501",
-    appId: "1:141226414501:web:0bfac36ca8590e8333d84c"
-  };
+  apiKey: "AIzaSyCVJISFJASvRrGNyuNGOcW6jF5QhgZwsOM",
+  authDomain: "filmderapp-621aa.firebaseapp.com",
+  projectId: "filmderapp-621aa",
+  storageBucket: "filmderapp-621aa.appspot.com",
+  messagingSenderId: "141226414501",
+  appId: "1:141226414501:web:0bfac36ca8590e8333d84c"
+};
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
-
-//IOS: 232785738503-ff7ppofaf3a05ugce4ovgaethh4e5r4u.apps.googleusercontent.com
-//Android: 232785738503-br9ekjvd19tqh94hsq13cbk5q5ei1res.apps.googleusercontent.com
+export const auth = getAuth(app);
