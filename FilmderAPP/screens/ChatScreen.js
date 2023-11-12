4dbc20287b69b53258a07b4405e8be34a641a10a
vip71 <107436192+vip71@users.noreply.github.com>
2023-11-12 14:52:54 +0100

    feat: chat screen prototype

--- FilmderAPP/screens/ChatScreen.js
+++ FilmderAPP/screens/ChatScreen.js
@@ -1,12 +1,53 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { GiftedChat } from 'react-native-gifted-chat';
+import {auth} from "../FirebaseConnection";
 
-const ChatScreen = () => {
-    return (
-        <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-red-500">ChatScreen</Text>
-        </View>
-    )
+class Chat extends React.Component {
+    static navigationOptions = ({ navigation }) => ({
+        title: (navigation.state.params || {}).name || 'Chat!',
+    });
+
+    state = {
+        messages: [
+            {
+                _id: 1,
+                text: 'Hello!',
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: 'React Native',
+                    avatar: 'https://placeimg.com/140/140/any',
+                },
+            },
+            {
+                _id: 2,
+                text: 'How are you?',
+                createdAt: new Date(),
+                user: {
+                    _id: 1,
+                    name: 'User',
+                    avatar: 'https://placeimg.com/140/140/any',
+                },
+            },
+        ],
+    };
+
+    onSend = newMessages => {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, newMessages),
+        }));
+    };
+
+    render() {
+        return (
+            <GiftedChat
+                user={{_id: auth?.currentUser?.email,name: auth?.currentUser?.displayName,avatar: auth?.currentUser?.photoURL}}
+                showAvatarForEveryMessage={true}
+                messages={this.state.messages}
+                onSend={messages => this.onSend(messages)}
+            />
+        );
+    }
 }
 
-export default ChatScreen
+export default Chat;
